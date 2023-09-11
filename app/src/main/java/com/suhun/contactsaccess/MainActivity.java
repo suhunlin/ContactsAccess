6538f3ee1eda85b3078661a7f31e85742d31286a
suhunlin <lin.suhun@msa.hinet.net>
2023-09-11 18:30:06 +0800

    Implement read phone book and show phone book to ListView

--- app/src/main/java/com/suhun/contactsaccess/MainActivity.java
+++ app/src/main/java/com/suhun/contactsaccess/MainActivity.java
@@ -1,8 +1,16 @@
 package com.suhun.contactsaccess;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -17,16 +25,54 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<HashMap<String, String>> data = new ArrayList<>();
     private String[] from = {"itemKey"};
     private int[] to = {R.id.contact_item};
+    private ContentResolver contentResolver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initListView();
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED){
+            //user have already agree access contact
+            contentResolver = getContentResolver();
+        }else{
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, 123);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 123){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                //user have already agree access contact
+                contentResolver = getContentResolver();
+            }else{
+                finish();
+            }
+        }
     }
 
     public void getContactFun(View view){
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        Cursor cursor = contentResolver.query(uri, null, null, null, null);
 
+        while (cursor.moveToNext()){
+            String name = cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String phone = cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            try {
+                String namePresent = name.replace(name.charAt(1), '0');
+                String phonePresent = phone.substring(0, 7);
+                String result = String.format("%s:%s", namePresent, phonePresent);
+                HashMap<String, String> contant = new HashMap<>();
+                contant.put(from[0], result);
+                data.add(contant);
+                simpleAdapter.notifyDataSetChanged();
+            }catch (Exception e){
+
+            }
+        }
+        cursor.close();
     }
 
     private void initListView(){
